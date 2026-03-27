3c326a498d227fa43c19edd0574d14ab477cb8a4
user <user@users-MacBook-Pro.local>
2026-03-27 23:04:05 +0400

    fix

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "shopdb",
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool;
