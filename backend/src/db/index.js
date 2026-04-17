e20181591d55df2a0f29826a76bdfa4e2bf39418
Victor <petrosyan.victor05@gmail.com>
2026-04-18 01:02:17 +0400

    add CI

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,16 +1,10 @@
 import { Pool } from "pg";
 
-console.log(`host: ${process.env.DB_HOST},
-  port: ${process.env.DB_PORT},
-  database: ${process.env.DB_NAME},
-  user: ${process.env.DB_USER},
-  password: ${process.env.DB_PASSWORD}`)
 
 export const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT), // 👈 better to cast to number
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
-
